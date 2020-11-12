031cef5c4f40aa505005ca2b9df57b942ab92e91
hunghly <hunghly@gmail.com>
2020-11-12 14:51:15 -0600

    added initial modules for testing

---
+++ headers/random.h
@@ -0,0 +1,1 @@
+#define RANDOM 131
---
+++ sources/main.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include "other.h"
+#include "random.h"
+
+int main(void) {
+
+    printf("My favorite number is: %d\n", getfavoritenumber());
+
+}
