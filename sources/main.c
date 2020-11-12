bd59a77765be93811e6242174ced53e9ccb402e5
hunghly <hunghly@gmail.com>
2020-11-12 16:02:09 -0600

    added extern/static vars

--- sources/main.c
+++ sources/main.c
@@ -2,8 +2,12 @@
 #include "other.h"
 #include "random.h"
 
+extern int globalVar; // access a global variable using extern keyword (from other.c)
+
 int main(void) {
 
     printf("My favorite number is: %d\n", getfavoritenumber());
+    printf("Printing global var %d\n", globalVar);
+
 
 }
--- sources/other.c
+++ sources/other.c
@@ -1,5 +1,8 @@
 #include "other.h"
 
+int globalVar = 35; // declare a global variable
+static int notOutsideVar = 30; // this is a global variable that is not accessible by extern keyword or to other files
+
 int getfavoritenumber(void) {
     return 3;
 }
