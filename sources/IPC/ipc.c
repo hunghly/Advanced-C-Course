ee6ff5ca3b62e85657c42635e4939b284633851f
hunghly <hunghly@gmail.com>
2021-02-01 16:43:46 -0600

    added IPC need to work more on it

---
+++ sources/IPC/ipc.c
@@ -0,0 +1,10 @@
+
+
+int main(void) {
+    // a process is a program in execution
+    // a program is a file containing the information of a process
+    // each process has its own address space and usually one thread of control unlike threads that share address space
+    // you can have multiple processess executing the same program, but each process has ites own copy of the program within its own address space i.e. two open word documents do not share variables
+
+    return 0;
+}
