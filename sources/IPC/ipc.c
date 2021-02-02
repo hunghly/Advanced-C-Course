00e761fd7dbb0b2ac9388d3e291701546d112377
hunghly <hunghly@gmail.com>
2021-02-02 16:25:12 -0600

    added raising a signal

--- sources/IPC/ipc.c
+++ sources/IPC/ipc.c
@@ -5,6 +5,8 @@ int main(void) {
     // a program is a file containing the information of a process
     // each process has its own address space and usually one thread of control unlike threads that share address space
     // you can have multiple processess executing the same program, but each process has ites own copy of the program within its own address space i.e. two open word documents do not share variables
-
+    // a process created by a given parent are called child processes and a child can inherit many of it's attributes from the parent process
+    // each process is identified with a unique positive integer called a process id (PID)
+    // the system call getpid() returns the process IDof the calling process
     return 0;
 }
