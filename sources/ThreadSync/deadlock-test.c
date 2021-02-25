942648266776861f2343cb3c38c9e0f903f40bfd
hunghly <hunghly@gmail.com>
2021-02-25 17:12:55 -0600

    finished deadlocks

--- sources/ThreadSync/deadlock-test.c
+++ sources/ThreadSync/deadlock-test.c
@@ -11,10 +11,25 @@ void *resource1() {
     sleep(2);
 
     printf("Trying to get resource2...\n");
-    pthread_mutex_lock(&lock2);
+    // pthread_mutex_lock(&lock2); // this will cause a deadlock
+
+    // instead we can use trylock since it doesn't block
+    int i = pthread_mutex_trylock(&lock2);
+    // if pthread_mutex_trylock returns a 0 that means there was no thread holding he lock at the time
+    printf("i is %d\n", i);
+
+    while (i == 0) {
+        pthread_mutex_unlock(&lock1);
+        sleep(2);
+        pthread_mutex_lock(&lock1);
+    }
 
     printf("Acquired resource2\n");
     pthread_mutex_unlock(&lock2);
+
+    // do critical stuff
+
+    sleep(2);
 
     printf("Job finished in resource1...\n");
     pthread_mutex_unlock(&lock1);
@@ -30,6 +45,10 @@ void *resource2() {
     printf("Trying to get resource1...\n");
     pthread_mutex_lock(&lock1);
 
+    printf("Acquired resource1\n");
+    pthread_mutex_unlock(&lock1);
+
+
     printf("Job finished in resource2...\n");
     pthread_mutex_unlock(&lock2);
     pthread_exit(NULL);
@@ -37,5 +56,16 @@ void *resource2() {
 
 int main(void) {
 
+    pthread_mutex_init(&lock1, NULL);
+    pthread_mutex_init(&lock2, NULL);
+
+    pthread_t t1, t2;
+
+    pthread_create(&t1, NULL, resource1, NULL);
+    pthread_create(&t2, NULL, resource2, NULL);
+
+    pthread_join(t1, NULL);
+    pthread_join(t2, NULL);
+    
     return 0;
 }
